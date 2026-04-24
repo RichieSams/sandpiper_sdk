2b7f5fd1a5e403133ef28b052c82ac2161ceb39d
Adrian Astley <adastley@gmail.com>
2026-04-23 22:50:59 -0400

    feat: Initial electro_swing setup

---
+++ samples/electro_swing/assets.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+extern const uint32_t colorPalette[256];
+
+#define WATER_BALL_W 64
+#define WATER_BALL_H 64
+#define WATER_BALL_FRAME_COUNT 12
+
+extern const uint8_t *const waterBallSprite[WATER_BALL_FRAME_COUNT];
